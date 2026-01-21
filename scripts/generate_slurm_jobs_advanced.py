faab58e8fba460d6d75382799ce6ddae49b63f99
Ruike Lyu (吕睿可) <47710448+Rick10119@users.noreply.github.com>
2026-01-21 17:13:28 -0500

    20G per node

--- scripts/generate_slurm_jobs_advanced.py
+++ scripts/generate_slurm_jobs_advanced.py
@@ -167,7 +167,7 @@ class SlurmJobGenerator:
             "nodes": 1,
             "ntasks": 1,
             "cpus_per_task": 32,
-            "mem_per_cpu": "15G",
+            "mem_per_cpu": "20G",
             "time_limit": "16:00:00",
             "mail_user": "rl8728@princeton.edu",
             "modules": [
